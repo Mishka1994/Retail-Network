43313e3faf9780fd248c0682d44508784341b659
mihail samofalov <misasamofalov611@gmail.com>
2024-04-13 20:55:20 +0700

    create models Products and Factory

--- dealers/models.py
+++ dealers/models.py
@@ -29,3 +29,31 @@ class Contacts(models.Model):
         verbose_name = 'Контакт'
         verbose_name_plural = 'Контакты'
 
+
+class Products(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название продукта')
+    model = models.CharField(max_length=150, verbose_name='Модель продукта')
+    market_launch_date = models.DateTimeField(verbose_name='Дата выхода на рынок')
+
+    def __str__(self):
+        return f'{self.title} - ({self.model}, {self.market_launch_date})'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Factory(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название завода')
+    contacts = models.ForeignKey(Contacts, on_delete=models.CASCADE, verbose_name='Контакты')
+    products = models.ForeignKey(Products, on_delete=models.PROTECT, verbose_name='Продукты')
+    provider = models.OneToOneField('self', on_delete=models.CASCADE, verbose_name='Поставщик')
+    time_to_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    debt = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Задолженность', default=0)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'завод'
+        verbose_name_plural = 'заводы'
